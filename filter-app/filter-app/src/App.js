29c79aa967e5931fd3e4f6bc1a9e3aaa945e9dba
M0lt0 <bodymgahed@yahoo.com>
2024-03-27 23:33:25 -0300

    static version

--- filter-app/filter-app/src/App.js
+++ filter-app/filter-app/src/App.js
@@ -1,16 +1,15 @@
-function productCategoryRow({ category }) {
+function ProductCategoryRow({ category }) {
   return (
     <tr>
-      <th colSpan="2">
+      <th colSpan="4">
         {category}
-
       </th>
     </tr>
   )
 
 }
 
-function productRow({ product }) {
+function ProductRow({ product }) {
   const name = product.stocked ? product.name :
     <span style={{ color: 'red' }}>
       {product.name}
@@ -24,13 +23,73 @@ function productRow({ product }) {
   )
 }
 
-function productTable({ products }) {
-  const row = []
+function ProductTable({ products }) {
+  const rows = []
   let lastCategory = null
 
   products.forEach((product) => {
-    if (product.category) { }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      )
 
-  });
+    }
+
+    rows.push(
+      <ProductRow product={product} key={product.name} />
+    )
+
+    lastCategory = product.category
+  })
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="search..." />
+      <label >
+        <input type="checkbox" />
+        {' '}
+        only show product in stock
+      </label>
+    </form>
+  )
+
+}
+
+function FilterProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  )
+
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+]
+
+export default function App() {
+  return <FilterProductTable products={PRODUCTS} />
 
 }
